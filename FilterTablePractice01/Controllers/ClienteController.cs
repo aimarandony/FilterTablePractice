bc05c6ed2dfef10af0e5de268822a124466f3149
Aimar Andony <aimarandony@gmail.com>
2019-11-26 08:55:15 -0500

    second commit

--- FilterTablePractice01/Controllers/ClienteController.cs
+++ FilterTablePractice01/Controllers/ClienteController.cs
@@ -80,5 +80,40 @@ namespace FilterTablePractice01.Controllers
             ViewBag.fecha = fecha;
             return View(ClientesFecha(fecha));
         }
+
+        List<Cliente> ClientesNombreFecha(string nombre, int fecha)
+        {
+            List<Cliente> temporal = new List<Cliente>();
+
+            SqlCommand cmd = new SqlCommand("USP_CLIENTES_NOMBRE_FECHA", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@nombre", nombre);
+            cmd.Parameters.AddWithValue("@fecha", fecha);
+
+            cn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Cliente reg = new Cliente
+                {
+                    idcli = dr.GetInt32(0),
+                    nomcli = dr.GetString(1),
+                    apecli = dr.GetString(2),
+                    nomdist = dr.GetString(3),
+                    fecha_reg = dr.GetDateTime(4),
+                    estado = dr.GetInt32(5),
+                };
+                temporal.Add(reg);
+            }
+            dr.Close(); cn.Close();
+            return temporal;
+        }
+
+        public ActionResult ClientesporNombreFecha(string nombre = "", int fecha = 0)
+        {
+            ViewBag.nombre = nombre;
+            ViewBag.fecha = fecha;
+            return View(ClientesNombreFecha(nombre, fecha));
+        }
     }
 }
